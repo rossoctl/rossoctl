66edb3ab02c374d77d1880c71932ac36d6ffe2e8
Ed Snible <snible@us.ibm.com>
2026-07-27 16:57:10 -0400

    Use lower-case RGB hex colors
    Signed-off-by: Ed Snible <snible@us.ibm.com>

--- rossoctl/ui-v2/src/components/AppLayout.tsx
+++ rossoctl/ui-v2/src/components/AppLayout.tsx
@@ -222,7 +222,7 @@ export const AppLayout: React.FC<AppLayoutProps> = ({ children, features }) => {
             viewBox="0 0 100 100"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <rect width="100" height="100" rx="20" fill="#CC0000" />
+            <rect width="100" height="100" rx="20" fill="#cc0000" />
             <text
               x="50"
               y="68"
